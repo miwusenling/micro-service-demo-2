35851ee4835349a4f40e6a3464e2d8f342bb34df
miwusenling <f-16cd@163.com>
2022-05-16 12:47:13 +0800

    20220516: dev branch --> add 555,666 in Controller.java

--- src/main/java/com/zhenwen/springboot/Controller.java
+++ src/main/java/com/zhenwen/springboot/Controller.java
@@ -13,6 +13,8 @@ public class Controller {
 		System.out.println("111");
 		System.out.println("333");
 		System.out.println("444");
+		System.out.println("555");
+		System.out.println("666");
         return "hello spring boot";
     }
 
